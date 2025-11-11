2c1acd6e881a0a1ea811739050d15e2653f46a3d
Olle Mattsson <rymdolle@users.noreply.github.com>
2025-11-11 16:27:33 +0100

    Pokemon abstract class

---
+++ Pokemon.Game/Pokemon.cs
@@ -0,0 +1,45 @@
+﻿namespace Pokemon.Game;
+
+internal abstract class Pokemon(string name, List<Attack> attacks)
+{
+    private string _name = name;
+    private int _level = 1;
+
+    public string Name {
+        get => _name;
+        set
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(value, "Pokemon name cannot be null or empty.");
+            if (value.Length < 2 || value.Length > 15)
+                throw new ArgumentOutOfRangeException("Pokemon name must be between 2 and 15 characters long.");
+            _name = value;
+        }
+    }
+    public int Level
+    {
+        get => _level;
+        set
+        {
+            ArgumentOutOfRangeException.ThrowIfLessThan(value, 1);
+            _level = value;
+        }
+    }
+
+    public ElementType Type { get; set; }
+    public List<Attack> Attacks { get; } = attacks;
+
+    public void RandomAttack()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Attack()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RaiseLevel()
+    {
+        throw new NotImplementedException();
+    }
+}

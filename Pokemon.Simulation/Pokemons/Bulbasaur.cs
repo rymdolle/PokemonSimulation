80ffedd37618254ea92ec766c6a30cee7654acc5
Olle Mattsson <rymdolle@users.noreply.github.com>
2025-11-12 10:49:25 +0100

    Evolve Bulbasaur and Squirtle

--- Pokemon.Simulation/Pokemons/Bulbasaur.cs
+++ Pokemon.Simulation/Pokemons/Bulbasaur.cs
@@ -1,6 +1,13 @@
 ﻿namespace Pokemon.Simulation.Pokemons;
 
 internal class Bulbasaur(List<Attack> attacks)
-    : GrassPokemon("Bulbasaur", attacks)
+    : GrassPokemon("Bulbasaur", attacks), IEvolvable
 {
+    public void Evolve()
+    {
+        string oldName = Name;
+        Name = "Ivysaur";
+        Level += 10;
+        Console.WriteLine($"{oldName} is evolving into {Name} and is now level {Level}!");
+    }
 }
--- Pokemon.Simulation/Pokemons/Squirtle.cs
+++ Pokemon.Simulation/Pokemons/Squirtle.cs
@@ -1,6 +1,13 @@
 ﻿namespace Pokemon.Simulation.Pokemons;
 
 internal class Squirtle(List<Attack> attacks)
-    : WaterPokemon("Squirtle", attacks)
+    : WaterPokemon("Squirtle", attacks), IEvolvable
 {
+    public void Evolve()
+    {
+        string oldName = Name;
+        Name = "Wartortle";
+        Level += 10;
+        Console.WriteLine($"{oldName} is evolving into {Name} and is now level {Level}!");
+    }
 }

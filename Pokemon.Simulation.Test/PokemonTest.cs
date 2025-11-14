45d9e186214d44c4ead8eb2d99f82e07d302b2b7
Olle Mattsson <rymdolle@users.noreply.github.com>
2025-11-14 12:28:24 +0100

    Add tests

---
+++ Pokemon.Simulation.Test/PokemonTest.cs
@@ -0,0 +1,21 @@
+﻿using Pokemon.Simulation.Pokemons;
+
+namespace Pokemon.Simulation.Test;
+
+public class PokemonTest
+{
+    [Fact]
+    public void RaiseLevel_IncrementAndEvolve()
+    {
+        // Arrange
+        Pokemon bulbasaur = new Bulbasaur([], level: 1);
+        int evolveAt = (bulbasaur as IEvolvable)!.EvolveAtLevel;
+
+        // Act
+        for (int i = bulbasaur.Level; i < evolveAt; i++)
+            bulbasaur = bulbasaur.RaiseLevel();
+
+        // Assert
+        Assert.Equal("Ivysaur", bulbasaur.Name);
+    }
+}

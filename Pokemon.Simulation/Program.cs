474027e9dc9ef04a886ae1a015c6c7e5f7e844c3
Olle Mattsson <rymdolle@users.noreply.github.com>
2025-11-14 10:57:17 +0100

    Use named arguments

--- Pokemon.Simulation/Program.cs
+++ Pokemon.Simulation/Program.cs
@@ -6,17 +6,17 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Attack scratch = new("Scratch", ElementType.Normal, 16);
-        Attack ember = new("Ember", ElementType.Fire, 14);
-        Attack tackle = new("Tackle", ElementType.Normal, 12);
-        Attack vineWhip = new("Vine Whip", ElementType.Grass, 16);
-        Attack waterGun = new("Water Gun", ElementType.Water, 16);
+        Attack scratch = new("Scratch", ElementType.Normal, power: 16);
+        Attack ember = new("Ember", ElementType.Fire, power: 14);
+        Attack tackle = new("Tackle", ElementType.Normal, power: 12);
+        Attack vineWhip = new("Vine Whip", ElementType.Grass, power: 16);
+        Attack waterGun = new("Water Gun", ElementType.Water, power: 16);
 
         List<Pokemon> pokemons = [
-            new Charmander([scratch, ember], 1),
-            new Bulbasaur([tackle, vineWhip], 4),
-            new Squirtle([tackle, waterGun], 1),
-            new Snorlax([tackle], 1),
+            new Charmander(attacks: [scratch, ember], level: 1),
+            new Bulbasaur(attacks: [tackle, vineWhip], level: 4),
+            new Squirtle(attacks: [tackle, waterGun], level: 1),
+            new Snorlax(attacks: [tackle], level: 1),
         ];
 
         for (int i = 0; i < pokemons.Count; i++)
